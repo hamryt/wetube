aa5396791c35be737d6991936fcb79884bb060b2
hamryt <rkdl0829@naver.com>
2021-01-19 00:00:38 +0900

    modeling

--- init.js
+++ init.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();//  가리고 싶은 정보를 가리는 것
 import "./models/Video";
 import "./models/Comment"
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
